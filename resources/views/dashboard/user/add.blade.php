bbe3f08b60c64b06b5537eaac7526d203298cf4a
yandiyandhi <yandiyandhi1294@gmail.com>
2023-06-19 20:34:59 +0700

    two

--- resources/views/dashboard/user/add.blade.php
+++ resources/views/dashboard/user/add.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard.layout.index')
 @section('cont')
     <div class="row">
-        <form action="/dashboard/listuser" method="post">
+        <form action="/dashboard/listuser" method="post" enctype="multipart/form-data">
             @csrf
             <div class="card">
                 <div class="card-header bg-transparent " style="background: #3C91E6 !important;">
@@ -57,61 +57,6 @@
                 <!-- /.card-body -->
             </div>
         </form>
-
-        {{-- <form method="POST" action="/dashboard/listuser" class="mb-5" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">Nama</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
-                    name="title" required autofocus value="{{ old('title') }}">
-                @error('title')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="slug" class="form-label">Slug</label>
-                <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug"
-                    required value="{{ old('slug') }}">
-                @error('title')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="Category" class="form-label">Category</label>
-                <select class="form-select" name="category_id">
-                    @foreach ($categories as $category)
-                        @if (old('category_id') == $category->id)
-                            <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
-                        @else
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="image" class="form-label @error('image') is-invalid @enderror">Post Image</label>
-                <img class="img-preview img-fluid mb-3 col-sm-5">
-                <input class="form-control" type="file" id="image" name="image" onchange="previewImage()">
-                @error('image')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label for="body" class="form-label">Body</label>
-                @error('body')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-                <input id="body" type="hidden" name="body" value="{{ old('body') }}">
-                <trix-editor input="body"></trix-editor>
-            </div>
-            <button type="submit" class="btn btn-primary">Create Post</button>
-        </form> --}}
 
     </div>
 
